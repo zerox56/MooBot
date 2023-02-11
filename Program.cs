563f74ac1d96b8e6cfd682f7ece21344af0fc6b1
Zerox <patrick.eijkemans@gmail.com>
2023-02-11 21:55:05 +0100

    Add DiscordManager

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using dotenv.net;
+using Managers.DiscordManager;
+
+class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private async Task MainAsync()
+    {
+        DotEnv.Load();
+
+        DiscordManager discordManager = new DiscordManager();
+        await discordManager.MainAsync();
+    }
+}

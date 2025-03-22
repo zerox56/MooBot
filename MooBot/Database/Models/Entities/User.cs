f68a6147dcbc68c3ea4b4e2f20b9987d23950590
zerox56 <zerox56@live.nl>
2025-03-22 16:19:03 +0100

    Add option to disable being pinged for assignees

--- MooBot/Database/Models/Entities/User.cs
+++ MooBot/Database/Models/Entities/User.cs
@@ -5,6 +5,9 @@ namespace Moobot.Database.Models.Entities
     [Table("users")]
     public class User : Entity
     {
+        [Column("ping_for_assignees")]
+        public bool PingForAssignees { get; set; }
+
         [Column("user_reminder")]
         public ICollection<UserReminder> UserReminders { get; set; }
     }

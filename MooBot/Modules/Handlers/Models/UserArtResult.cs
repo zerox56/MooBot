650ca17ce1ebb994ca880fe61efbe1c30e09c0dd
zerox56 <zerox56@live.nl>
2025-05-27 19:58:54 +0200

    Add extra descriptions

--- MooBot/Modules/Handlers/Models/UserArtResult.cs
+++ MooBot/Modules/Handlers/Models/UserArtResult.cs
@@ -1,8 +1,11 @@
-﻿namespace MooBot.Modules.Handlers.Models
+﻿using MooBot.Modules.Handlers.Models.AutoAssign;
+
+namespace MooBot.Modules.Handlers.Models
 {
     public class UserArtResult
     {
         public string ImageUrl { get; set; }
+        public Character SelectedCharacter { get; set; }
         public string[] Characters { get; set; }
         public string[] Artists { get; set; }
     }

f5ccbc8091ccb0ff38f9a40b26fd853437a49179
zerox56 <zerox56@live.nl>
2025-09-14 16:44:00 +0200

    Add auto fix domain group urls

--- MooBot/Managers/MessageManager.cs
+++ MooBot/Managers/MessageManager.cs
@@ -1,10 +1,13 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 using Moobot.Database;
 using Moobot.Database.Models.Entities;
 using Moobot.Database.Queries;
 using Moobot.Managers;
-using MooBot.Configuration;
+using Moobot.Utils;
+using MooBot.Managers.Enums;
 using MooBot.Modules.Handlers;
+using System.Text.RegularExpressions;
 
 namespace MooBot.Managers
 {
@@ -20,10 +23,77 @@ namespace MooBot.Managers
             var channel = await dbContext.Channel.GetChannelById(msg.Channel.Id, guildId);
 
             if (channel == default(Channel)) return;
-            if (!channel.CheckAssignees) return;
+
+            var cleanedLink = await CleanupTwitterLink(msg);
+
+            if (!channel.CheckAssignees || cleanedLink) return;
 
             //TODO: Check if this even has to be awaited
             await AutoAssignHandler.AutoAssignCharacters(msg);
+        }
+
+        private static async Task<bool> CleanupTwitterLink(SocketMessage msg) {
+            var contentUrls = StringUtils.GetAllUrls(msg.Content);
+
+            if (contentUrls.Length == 0) return false;
+
+            var containsSpoiler = StringUtils.CountOccurrences(msg.Content, "||") >= 2 || msg.Attachments.Any(a => a.IsSpoiler());
+            var dbContext = ServiceManager.GetService<DatabaseContext>();
+
+            var responseMessage = "";
+            var validUrls = false;
+            var hasQueryInUrl = false;
+
+            foreach (var conentUrl in contentUrls)
+            {
+                if (!Uri.TryCreate(conentUrl, UriKind.Absolute, out var uri)) continue;
+
+                var url = uri.AbsoluteUri;
+                if (!string.IsNullOrEmpty(uri.Query))
+                {
+                    url = uri.GetLeftPart(UriPartial.Path);
+                    hasQueryInUrl = true;
+                }
+
+                var host = uri.Host.StartsWith("www.") ? uri.Host[4..] : uri.Host;
+                DomainGroup domainGroup = await dbContext.DomainGroup.GetDomainGroupById(host);
+
+                if (domainGroup == default(DomainGroup)) continue;
+
+                switch (domainGroup.Group)
+                {
+                    case DomainGroupEnum.Twitter:
+                        validUrls = true;
+                        var match = Regex.Match(uri.AbsolutePath, @"^/([^/]+)/status/(\d+)");
+                        if (!match.Success) break;
+
+                        var username = match.Groups[1].Value;
+                        var tweetId = match.Groups[2].Value;
+
+                        responseMessage += $"https://fixupx.com/{username}/status/{tweetId} ";
+                        
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (!validUrls) return false;
+
+            responseMessage.Trim();
+            var channel = msg.Channel as IMessageChannel;
+
+            if (hasQueryInUrl)
+            {
+                responseMessage = $"Nice tracker{Environment.NewLine}{responseMessage}";
+            }
+
+            await channel.SendMessageAsync(
+                text: responseMessage,
+                messageReference: new MessageReference(msg.Id)
+            );
+
+            return true;
         }
     }
 }
